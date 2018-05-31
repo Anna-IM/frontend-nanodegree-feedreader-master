d63324df8ad3e2044f1eb74853b84f69b80aa9b3
Anna <ilieva.anna@abv.bg>
2018-05-31 10:19:37 +0200

    Add existing file

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -76,24 +76,28 @@ $(function() {
       expect($('body').hasClass('menu-hidden')).toBe(true);
     });
   });
-  
+
   /* TODO: Write a new test suite named "Initial Entries" */
   describe('Initial Entries', function() {
-    beforeEach(function(done) {
-      loadFeed(0);
-      done();
-    });
+
     /* TODO: Write a test that ensures when the loadFeed
      * function is called and completes its work, there is at least
      * a single .entry element within the .feed container.
      * Remember, loadFeed() is asynchronous so this test will require
      * the use of Jasmine's beforeEach and asynchronous done() function.
      */
-    it('After loadFeed is loaded there is at least a single .entry within the .feed container', function(done) {
-      expect($('.entry')).not.toBe(0);
-      done();
+    beforeEach(function(done) {
+      loadFeed(0, function() {
+        done();
+      });
+    });
+
+    it('After loadFeed is loaded there is at least a single .entry within the .feed container', function() {
+      expect($('.feed').has('.entry').length).not.toBe(0);
+
     });
   });
+
   /* TODO: Write a new test suite named "New Feed Selection" */
   describe('New Feed Selection', function() {
     var loadingFeed;
